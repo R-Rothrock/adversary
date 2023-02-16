09ca0e979b43b3c32a2f7e75dc4c16d9fc5051e3
Roan Rothrock <roan.rothrock@hotmail.com>
2023-02-16 12:46:36 -0600

    Modified

--- test/window.py
+++ test/window.py
@@ -2,7 +2,19 @@
 # window.py
 # no dependencies but the standard library.
 
-import ctypes
+from ctypes import CDLL
 import tkinter
 
-dll = ctypes.LoadLibrary("./adversary.so")
+try:
+    dll = CDLL("./adversary.so")
+except FileNotFoundError:
+    print("'adversary.so' not found. Compiling now.")
+    import os
+    try:
+        os.system("source compile_dll")
+    except:
+        raise RuntimeError(
+            "compilation failed. https://github.com/R-Rothrock/adversary for more information"
+        )
+
+print(dll.play_round(2))

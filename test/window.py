04dffac78895d2b228521ea2456844a61acd8615
Roan Rothrock <roan.rothrock@hotmail.com>
2023-02-17 10:47:27 -0600

    progresson `test/window.py`

--- test/window.py
+++ test/window.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# window.py
-# no dependencies but the standard library.
 
 import ctypes
 import tkinter as tk
@@ -10,7 +8,7 @@ PAPER    = 2
 SCISSORS = 3
 OPTIONS  = (ROCK, PAPER, SCISSORS)
 
-player_guess = 0 # for now...
+player_guess = 0
 
 # loading shared object library
 # make sure .so of adversary is compiled and inside this directory
@@ -19,8 +17,6 @@ dll = ctypes.cdll.LoadLibrary("./adversary.so")
 
 def adversary(player_guess: int) -> int:
     """
-    RETURNS
-    ---
     - 1: computer wins ):
     - 2: you win (:
     - 3: tie ~}:
@@ -28,19 +24,28 @@ def adversary(player_guess: int) -> int:
     assert player_guess in OPTIONS
     return dll.adversary_round(player_guess)
 
-def guess_rock(player_guess=player_guess) -> None:
+# functions for button press
+
+def guess_rock() -> None:
+    global player_guess
     player_guess = ROCK
     print("[+] player guess is rock")
 
-def guess_paper(player_guess=player_guess) -> None:
+def guess_paper() -> None:
+    global player_guess
     player_guess = PAPER
     print("[+] player guess is paper")
 
-def guess_scissors(player_guess=player_guess) -> None:
+def guess_scissors() -> None:
+    global player_guess
     player_guess = SCISSORS
     print("[+] player guess is scissors")
 
-# now the GUI, courtesy of tkinter from the STL
+def eval() -> None:
+    global result
+    result = adversary(player_guess)
+
+# now the GUI
 
 win = tk.Tk()
 win.geometry("300x300")
@@ -70,8 +75,18 @@ tk.Button(
     win,
     text="Scissors",
     command=guess_scissors
-).grid(row=4, column=1, stick=tk.W)
+).grid(row=4, column=1, sticky=tk.W)
+
+# button for sending guess
+tk.Button(
+    win,
+    text="Go",
+    command=eval
+).grid(row=5, column=1, pady=10, stick=tk.W)
+
+tk.Label(
+    win,
+    textvariable=
+)
 
 win.mainloop()
-
-print(player_guess)
